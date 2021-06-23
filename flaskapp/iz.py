418002e249c5733d5f0a2d32bc51fa16fd1b4b72
Superbro513 <59731749+Superkotleta@users.noreply.github.com>
2021-06-23 23:21:07 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -81,9 +81,9 @@ def draw(filename,cho,rcolor,gcolor,bcolor):
  plt.close()
  
  img= np.array
- img[x/2,:,0] = rcolor
- img[x/2:,:,1] = gcolor
- img[x/2:,:,2] = bcolor
+ img[x//2,:,0] = rcolor
+ img[x//2:,:,1] = gcolor
+ img[x//2:,:,2] = bcolor
  
 ##меняем половинки картинок по выбору
  if cho==1: 
